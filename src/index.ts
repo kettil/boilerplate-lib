8018ec3c56fe19e5c446df50f3aec182a89196dd
Kettil <development@kettil.de>
2018-09-25 22:30:05 +0200

    add index.ts and types.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { optionsType } from './libs/types';
+
+export * from './libs/types';
+
+/**
+ *
+ * @param param0
+ */
+export default function main({  }: optionsType = {}) {
+  return 'hello world';
+}
